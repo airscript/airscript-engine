4962ccd830daea0596524ab62a6a5acac510ed8f
Jeff Lindsay <progrium@gmail.com>
2013-01-22 03:41:52 -0600

    i guess theres no none in lua?

--- helloworld.py
+++ helloworld.py
@@ -15,7 +15,7 @@ function(request)
     {0}
 end
 """.format(src.text))
-    query = collections.defaultdict(lambda: None)
+    query = collections.defaultdict(lambda: "")
     for k,v in request.args.items():
         query[k] = v
     req = dict(
