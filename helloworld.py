c5c535303b65ea392e7ebb9ecc655ad27d16d4a8
Jeff Lindsay <progrium@gmail.com>
2013-01-22 03:04:54 -0600

    using lupa

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,12 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+import lupa
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    lua = lupa.LuaRuntime()
+    return str(lua.eval("1+1"))
 
 if __name__ == "__main__":
     app.run('0.0.0.0', int(os.environ.get("PORT", 5000)))
