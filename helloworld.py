67536fcbb7167b545f0bd7ee839de78a69b52267
Jeff Lindsay <progrium@gmail.com>
2013-01-22 03:08:40 -0600

    lua helloworld

--- helloworld.py
+++ helloworld.py
@@ -7,7 +7,7 @@ import lupa
 @app.route("/")
 def hello():
     lua = lupa.LuaRuntime()
-    return str(lua.eval("1+1"))
+    return str(lua.eval("return \"Hello, World!\""))
 
 if __name__ == "__main__":
     app.run('0.0.0.0', int(os.environ.get("PORT", 5000)))
