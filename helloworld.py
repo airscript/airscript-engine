da5b760eddd2f0f438d9f526bae980d35f983286
Jeff Lindsay <progrium@gmail.com>
2013-01-22 03:13:20 -0600

    needs to be wrapped in a function

--- helloworld.py
+++ helloworld.py
@@ -7,7 +7,12 @@ import lupa
 @app.route("/")
 def hello():
     lua = lupa.LuaRuntime()
-    return str(lua.eval("return \"Hello, World!\""))
+    app = lua.eval("""
+function()
+    return "Hello, World!"
+end
+""")
+    return app()
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', int(os.environ.get("PORT", 5000)))
+    app.run('0.0.0.0', int(os.environ.get("PORT", 5000)), debug=True)
