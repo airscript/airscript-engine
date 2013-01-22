53aaddaea76275fcff8e4232d820f2f4c6d3572e
Jeff Lindsay <progrium@gmail.com>
2013-01-22 09:50:08 -0600

    best of both

--- runtime/__init__.py
+++ runtime/__init__.py
@@ -61,11 +61,11 @@ end
     
     globals = _export_globals()
     globals['request'] = adapt_request(request)
-    globals['require'] = require
-    #globals['require'] = lua.eval("""function(name)
-    #return loadstring(http.request({url="https://raw.github.com/webscriptio/lib/master/" .. name .. ".lua"}).content)()
-#end
-#""")
+    #globals['require'] = require
+    globals['require'] = lua.eval("""
+function(name) return
+    loadstring(http.load(name))
+end""")
     return adapt_response(
         app(lupa.as_attrgetter(globals)))
 
--- runtime/http.py
+++ runtime/http.py
@@ -6,3 +6,15 @@ def request(T):
         'content': res.text, 
         'statuscode': res.status_code, 
         'headers': {}}
+
+def load(path):
+    webscript_lib = "https://raw.github.com/webscriptio/lib/master/{0}.lua"
+    builtin = requests.get(webscript_lib.format(path))
+    if builtin.status_code == 200:
+        return builtin.text
+    user, repo, path = path.split('/', 2)
+    external_lib = "https://raw.github.com/{0}/{1}/master/{2}.lua"
+    external = requests.get(external_lib.format(user, repo, path))
+    if external.status_code == 200:
+        return external.text
+    return ""
