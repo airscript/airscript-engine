2f0bde24952abaf1f05cc5113ea1d199e434cd5a
Jeff Lindsay <progrium@gmail.com>
2013-01-22 04:01:02 -0600

    response building

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,11 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, make_response
 app = Flask(__name__)
 
 import lupa
 import requests
 import collections
+import json
 
 @app.route("/<path:path>")
 def hello(path):
@@ -21,7 +22,30 @@ end
     req = dict(
         query=query
     )
-    return app(req)
+    return build_response(app(req))
+
+def build_response(resp):
+    status = 200
+    headers = {"Content-Type": "text/plain"}
+    body = ""
+    body_set = False
+    for value in resp:
+        if isinstance(value, str):
+            body = value
+            body_set = True
+        elif isinstance(value, int):
+            status = value
+        elif not body_set and isinstance(value, dict):
+            body = json.dumps(value)
+            headers['Content-Type'] = 'application/json'
+            body_set = True
+        elif body_set and isinstance(value, dict):
+            for header in value:
+                headers[header] = value[header]
+    response = make_response(body, status)
+    for header in headers:
+        response.headers[header] = headers[header]
+    return response
 
 if __name__ == "__main__":
     app.run('0.0.0.0', int(os.environ.get("PORT", 5000)), debug=True)
