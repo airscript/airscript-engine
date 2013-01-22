3ad6d63fde1a205856a06a897fd05823beb7c0c3
Jeff Lindsay <progrium@gmail.com>
2013-01-22 02:57:07 -0600

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', int(os.environ.get("PORT", 5000)))
